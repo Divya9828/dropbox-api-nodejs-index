1249438fdbf95fe5900a5ca7aadbb1f05c6c287f
Divya <vebbox.divya@gmail.com>
2023-03-14 15:36:24 +0530

    all folder and file retrieve
    all folder and file

--- index.js
+++ index.js
@@ -16,14 +16,41 @@ var fetch = require("isomorphic-fetch"); // or another library of choice.
 var Dropbox = require("dropbox").Dropbox;
 
 const dbx = new Dropbox({ 
-  clientId: 'c3458eppqfffipb',
-  clientSecret: 'nhxm9uo7r1wodnn',
-  refreshToken: 'mjcX8muY27gAAAAAAAAAAX8Rq5HfDBqaBIqQIumGebw8zZLwnH_MVBTT43eeC_2K'
+  clientId: '1qn6e332jh9hak8',
+  clientSecret: '1tl3agf65eshbnv',
+  refreshToken: '63tjLcof9gIAAAAAAAAAASlNWnP5gWm1FBai4pglKuw8WGRyKmGxXo3rXN4scgkz'
 });
 app.get('/',(req,res)=>
 {
-  res.send("hello")
+  output().then((result) => {
+    res.send(result);
+  });
+  // res.send("hello")
 })
+async function output() {
+  // console.log("out");
+  j = j + 1;
+  await dbx.filesListFolder({ path: path_name }).then(async (response) => {
+    console.log(response.result.entries);
+    response.result.entries.map((data) => {
+      if (data[".tag"] == "file") {
+        file_path.push(data.path_lower);
+      }
+      if (data[".tag"] == "folder") {
+        folder_path.push(data.path_lower);
+      }
+    });
+    path_name = folder_path[j];
+    if (j < folder_path.length) {
+      await output().then((result) => {
+        // return {folder_path,file_path}
+        return [folder_path, file_path];
+      });
+    }
+  });
+  // return {folder_path,file_path}
+  return [folder_path, file_path];
+}
 app.get("/check", (req, res) => {
   // var dbx = new Dropbox({
   //   accessToken:
